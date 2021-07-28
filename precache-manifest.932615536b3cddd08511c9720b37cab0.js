71eda9db90f9316dbc270ba912a637fe09dfb929
atvars velavs <atvarsvelavs@atvarss-MBP.broadband>
2021-07-28 23:38:08 +0100

    Updates

---
+++ precache-manifest.932615536b3cddd08511c9720b37cab0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fbec924c20edfad3d4eca42fa0d3f9",
+    "url": "/eventBrite/index.html"
+  },
+  {
+    "revision": "17fc3cb3ef1decc8daa1",
+    "url": "/eventBrite/static/css/main.de25be0d.chunk.css"
+  },
+  {
+    "revision": "6546f296586f9bda92b4",
+    "url": "/eventBrite/static/js/2.15cbad3f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/eventBrite/static/js/2.15cbad3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17fc3cb3ef1decc8daa1",
+    "url": "/eventBrite/static/js/main.902dc7f4.chunk.js"
+  },
+  {
+    "revision": "e281d576eb6bc5e247ec",
+    "url": "/eventBrite/static/js/runtime-main.b4fd7cd6.js"
+  }
+]);
